20cc71c40a584daac47598dac367c79243a1733f
polyhedra98 <1assassin19741@gmail.com>
2019-01-08 14:47:38 +0200

    In process of displaying recipes in home fr.

---
+++ app/src/main/java/com/mishenka/cookingstuff/adapters/RecipeAdapter.kt
@@ -0,0 +1,28 @@
+package com.mishenka.cookingstuff.adapters
+
+import android.content.Context
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import com.mishenka.cookingstuff.data.Recipe
+import android.app.Activity
+import android.widget.TextView
+import com.mishenka.cookingstuff.R
+
+
+class RecipeAdapter(context: Context, resource: Int, objects: List<Recipe>) : ArrayAdapter<Recipe>(context, resource, objects) {
+    val mResource = resource
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        val returnView = convertView ?: (context as Activity).layoutInflater.inflate(mResource, parent, false)
+
+        val recipe = getItem(position)
+        val tvRecipeName = returnView.findViewById<TextView>(R.id.tv_recipe_name)
+        val tvAuthorName = returnView.findViewById<TextView>(R.id.tv_author_name)
+        tvRecipeName.text = recipe?.name
+        tvAuthorName.text = recipe?.author
+
+        return returnView
+    }
+
+}
