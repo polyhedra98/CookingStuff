386348b1d1058176d2a18b052209cb642a019b09
polyhedra98 <1assassin19741@gmail.com>
2019-01-09 20:19:21 +0200

    Minor bug fixes

--- app/src/main/java/com/mishenka/cookingstuff/activities/AddRecipeActivity.kt
+++ app/src/main/java/com/mishenka/cookingstuff/activities/AddRecipeActivity.kt
@@ -1,8 +1,12 @@
 package com.mishenka.cookingstuff.activities
 
+import android.content.DialogInterface
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AlertDialog
+import android.view.View
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.ListView
 import android.widget.TextView
 import com.google.firebase.auth.FirebaseAuth
@@ -14,7 +18,8 @@ import com.mishenka.cookingstuff.data.Recipe
 import com.mishenka.cookingstuff.data.Step
 import com.mishenka.cookingstuff.utils.Utils
 
-class AddRecipeActivity : AppCompatActivity() {
+class AddRecipeActivity : AppCompatActivity(), StepsAdapter.StepListener {
+    private val mStepsList = arrayListOf(Step(), Step(), Step())
 
     private lateinit var messagesDBRef : DatabaseReference
     private lateinit var mStepsAdapter : StepsAdapter
@@ -24,7 +29,7 @@ class AddRecipeActivity : AppCompatActivity() {
         setContentView(R.layout.activity_add_recipe)
 
         messagesDBRef = FirebaseDatabase.getInstance().reference.child(Utils.CHILD_RECIPE)
-        mStepsAdapter = StepsAdapter(this, R.layout.item_step, arrayListOf(Step(), Step(), Step()))
+        mStepsAdapter = StepsAdapter(this, R.layout.item_step, mStepsList)
 
         //TODO("Reimplement as a RecycleView")
         val stepsList = findViewById<ListView>(R.id.lv_steps)
@@ -33,7 +38,7 @@ class AddRecipeActivity : AppCompatActivity() {
         //TODO("Make it a nice little '+' on the right side")
         val addStepButton = findViewById<Button>(R.id.b_add_step)
         addStepButton.setOnClickListener {
-            mStepsAdapter.add(Step())
+            mStepsList.add(Step())
             mStepsAdapter.notifyDataSetChanged()
         }
 
@@ -49,6 +54,45 @@ class AddRecipeActivity : AppCompatActivity() {
                 finish()
             }
         }
+    }
+
+    override fun onStepPicButtonClicked(v: View?, pv : View?, s : Step?) {
+        showPictureDialog()
+        v?.visibility = View.INVISIBLE
+        when (v?.id) {
+            R.id.b_first_step -> {
+                s?.firstButtonClicked = true
+                pv?.findViewById<ImageView>(R.id.iv_step_first)?.visibility = View.VISIBLE
+            }
+            R.id.b_second_step -> {
+                s?.secondButtonClicked = true
+                pv?.findViewById<ImageView>(R.id.iv_step_second)?.visibility = View.VISIBLE
+            }
+            R.id.b_third_step -> {
+                s?.thirdButtonClicked = true
+                pv?.findViewById<ImageView>(R.id.iv_step_third)?.visibility = View.VISIBLE
+            }
+        }
+    }
+
+    private fun showPictureDialog() {
+        val pictureDialog = AlertDialog.Builder(this)
+        pictureDialog.setTitle("Select Action")
+        val pictureDialogItems = arrayOf("Gallery", "Camera")
+        pictureDialog.setItems(pictureDialogItems) { dialog, which ->
+            when (which) {
+                0 -> choosePhotoFromGallery()
+                1 -> takePhotoFromCamera()
+            }
+        }
+        pictureDialog.show()
+    }
+
+    private fun choosePhotoFromGallery() {
+
+    }
+
+    private fun takePhotoFromCamera() {
 
     }
 
--- app/src/main/java/com/mishenka/cookingstuff/adapters/StepsAdapter.kt
+++ app/src/main/java/com/mishenka/cookingstuff/adapters/StepsAdapter.kt
@@ -12,6 +12,7 @@ import com.mishenka.cookingstuff.data.Step
 
 class StepsAdapter(context: Context, resource: Int, objects: List<Step>) : ArrayAdapter<Step>(context, resource, objects) {
     private val mResource = resource
+    private val mStepListener : StepListener? = if (context is StepListener) context else null
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
         val returnView = convertView ?: (context as Activity).layoutInflater.inflate(mResource, parent, false)
@@ -19,37 +20,37 @@ class StepsAdapter(context: Context, resource: Int, objects: List<Step>) : Array
         val step = getItem(position)
 
         val bFirstStep = returnView.findViewById<Button>(R.id.b_first_step)
-        bFirstStep.setOnClickListener(OnAddButtonClickListener(returnView))
+        bFirstStep.setOnClickListener(OnAddButtonClickListener(mStepListener, returnView, step))
         bFirstStep.visibility = if (step.firstButtonClicked) View.INVISIBLE else View.VISIBLE
+        val ivFirstStep = returnView.findViewById<ImageView>(R.id.iv_step_first)
+        ivFirstStep.visibility = bFirstStep.visibility.xor(View.INVISIBLE)
 
         val bSecondStep = returnView.findViewById<Button>(R.id.b_second_step)
-        bSecondStep.setOnClickListener(OnAddButtonClickListener(returnView))
+        bSecondStep.setOnClickListener(OnAddButtonClickListener(mStepListener, returnView, step))
         bSecondStep.visibility = if (step.secondButtonClicked) View.INVISIBLE else View.VISIBLE
+        val ivSecondStep = returnView.findViewById<ImageView>(R.id.iv_step_second)
+        ivSecondStep.visibility = bSecondStep.visibility.xor(View.INVISIBLE)
 
         val bThirdStep = returnView.findViewById<Button>(R.id.b_third_step)
-        bThirdStep.setOnClickListener(OnAddButtonClickListener(returnView))
+        bThirdStep.setOnClickListener(OnAddButtonClickListener(mStepListener, returnView, step))
         bThirdStep.visibility = if (step.thirdButtonClicked) View.INVISIBLE else View.VISIBLE
+        val ivThirdStep = returnView.findViewById<ImageView>(R.id.iv_step_third)
+        ivThirdStep.visibility = bThirdStep.visibility.xor(View.INVISIBLE)
 
         return returnView
     }
 
-    class OnAddButtonClickListener(parentView : View?) : View.OnClickListener {
+    class OnAddButtonClickListener(listener : StepListener?, parentView : View?, step : Step?) : View.OnClickListener {
+        private val mListener = listener
         private val mParentView = parentView
+        private val mStep = step
 
         override fun onClick(v: View?) {
-            //TODO("Start camera / gallery intent")
-            v?.visibility = View.INVISIBLE
-            when (v?.id) {
-                R.id.b_first_step -> {
-                    mParentView?.findViewById<ImageView>(R.id.iv_step_first)?.visibility = View.VISIBLE
-                }
-                R.id.b_second_step -> {
-                    mParentView?.findViewById<ImageView>(R.id.iv_step_second)?.visibility = View.VISIBLE
-                }
-                R.id.b_third_step -> {
-                    mParentView?.findViewById<ImageView>(R.id.iv_step_third)?.visibility = View.VISIBLE
-                }
-            }
+            mListener?.onStepPicButtonClicked(v, mParentView, mStep)
         }
+    }
+
+    interface StepListener {
+        fun onStepPicButtonClicked(v : View?, pv : View?, s : Step?)
     }
 }
